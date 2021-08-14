c22eb2459c0fa63d78cb75751ecc835cd05f5deb
Robert Lin <rcl2106@gmail.com>
2021-08-14 03:45:52 -0400

    ✨ Add `GCF cors proxy` to demo how to consume API from endpoint without `Access-Control-Allow-Origin` set

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
 const functions = require("firebase-functions")
+const cors = require('cors')({origin: true});
 require('isomorphic-fetch')
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true})
-  response.send("Hello from Firebase! It's Saturday! 🎉🥳")
+  cors(request, response, () => {
+    functions.logger.info("Hello logs!", {structuredData: true})
+    response.send("Testing CORS... It's Saturday! 🎉🥳")
+  })
 })
 
 exports.getDogs = functions.https.onRequest((request, response) => {
@@ -19,6 +22,29 @@ exports.getDogs = functions.https.onRequest((request, response) => {
       rs.json().then(data => {
         response.send(data)
       })
+    })
+  })
+})
+
+exports.getCtwcMatch = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    const url = 'https://ctwc-cloud.appspot.com/v1/match/ctwc_2014_r5-01'
+    fetch(url).then((rs) => {
+      if (rs.ok) {
+        return rs.json()
+      } else {
+        throw new Error('Something went wrong')
+      }
+    })
+    .then((responseJson) => {
+      // This also works but Google Firebase documentation suggests using the `cors` package
+      // https://stackoverflow.com/questions/42755131/enabling-cors-in-cloud-functions-for-firebase/42756623#42756623
+      // response.set("Access-Control-Allow-Origin", "*")
+      // response.set("Access-Control-Allow-Headers", "Content-Type")
+      response.send(responseJson)
+    })
+    .catch((error) => {
+      console.error(error)
     })
   })
 })
